d764da42f979eaa6007e3b7576973b0e9b13de64
Piwiik <piwik.u@hotmail.fr>
2017-04-27 11:08:38 +0200

    timeit

--- cdc/test.py
+++ cdc/test.py
@@ -3,10 +3,15 @@ from petit_catalogue import PETIT_CATALOGUE
 import timeit
 import os
 
+oui = charge_henri_draper("hd.dat.gz")
+temps1 = timeit.timeit(stmt="selection_champ_parcours_complet(oui,(50,50),20)",number=5,globals=globals())
+oui = charge_henri_draper("hd.dat.gz")
+temps2 = timeit.timeit(stmt="selection_champ_parcours_restreint(oui,(50,50),20)",number=5,globals=globals())
+print("Parcours complet :",temps1,"secondes\nParcours restreint :",temps2,"secondes")
 
+"""
 temps = timeit.timeit(stmt="os.system('python cdc.py -c 90 20 -r 30 -o svg wow.svg')", globals=globals(), number=5)
 print(temps)
-"""
 print()
 beh = reduit_parcours(ah,-0.5,0.5,1.5,2.5)
 print()
